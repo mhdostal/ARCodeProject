fc27d621ff1b6e06c51bd23466404a7d73891d0b
Mark Dostal <mhdostal@aol.com>
2019-12-04 14:38:17 -0600

    Initial XCode project

---
+++ ARCodeProject/ARCodeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARCodeProject
+//
+//  Created by Mark Dostal on 12/4/19.
+//  Copyright © 2019 Esri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
